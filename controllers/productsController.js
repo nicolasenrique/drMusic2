b6722cffc337a95697374625beff2ef35d8ff516
nicolasenrique <nicolas.enrique56@gmail.com>
2021-12-11 20:00:01 -0300

    add edit

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -104,47 +104,124 @@ const controlProducts = {
   },
   //
   edit: (req, res) => {
-    const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
-    let product;
-    for (prod of products) {
-      if (prod.prodId == req.params.id) {
-        product = prod;
-        break;
-      }
-    }
-    res.render("product_edit", { product: product });
+
+    // const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+    // let product;
+    // for (prod of products) {
+    //   if (prod.prodId == req.params.id) {
+    //     product = prod;
+    //     break;
+    //   }
+    // }
+    // res.render("product_edit", { product: product });
+
+    //
+
+    let productId = req.params.id;
+
+    let promProduct = db.Product.findByPk(productId,{include: ['color','prod_category','prod_size','prod_price','prod_image']});
+    let promColor = db.Color.findAll();
+    let promProdCat = db.ProdCategory.findAll();
+    let promProdPrice = db.ProdPrice.findAll();
+    let promProdSize = db.ProdSize.findAll();
+
+    Promise.all([promProduct, promColor, promProdCat, promProdPrice, promProdSize])
+      .then(([Products, allColors, allProdCats, allProdPrices, allProdSizes]) => {
+        return res.render(path.resolve(__dirname, "..", "views", "product_create"), {
+          Products,
+          allColors,
+          allProdCats,
+          allProdPrices,
+          allProdSizes
+        });
+      })
+      .catch((error) => res.send(error));
+
+
   },
   //
   update: (req, res) => {
     //
-    const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
-    let prodToUpadate = {};
-    for (i = 0; i < products.length; i++) {
-      //
-      if (products[i].prodId == req.body.prodId) {
-        prodToUpadate.prodId = req.body.prodId;
-        prodToUpadate.nombre = req.body.nombre;
-        prodToUpadate.descripcion = req.body.descripcion;
-        prodToUpadate.precio = req.body.precio;
-        if (req.file) {
-          prodToUpadate.imagen = req.file.filename;
-        } else {
-          prodToUpadate.imagen = products[i].imagen;
-        }
-        prodToUpadate.categoria = req.body.categoria;
-        prodToUpadate.medidas = req.body.medidas;
-        prodToUpadate.alto = req.body.alto;
-        prodToUpadate.ancho = req.body.ancho;
-        prodToUpadate.profundidad = req.body.profundidad;
-        prodToUpadate.color = req.body.color;
-        prodToUpadate.fechaCreacion = products[i].fechaCreacion;
-        prodToUpadate.fechaModificacion = new Date();
-        products[i] = prodToUpadate;
-        break;
-      }
-    }
-    grabaRegistros(products);
-    res.render("productDescription", { selectedProduct: prodToUpadate });
+    // const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+    // let prodToUpadate = {};
+    // for (i = 0; i < products.length; i++) {
+    //   //
+    //   if (products[i].prodId == req.body.prodId) {
+    //     prodToUpadate.prodId = req.body.prodId;
+    //     prodToUpadate.nombre = req.body.nombre;
+    //     prodToUpadate.descripcion = req.body.descripcion;
+    //     prodToUpadate.precio = req.body.precio;
+    //     if (req.file) {
+    //       prodToUpadate.imagen = req.file.filename;
+    //     } else {
+    //       prodToUpadate.imagen = products[i].imagen;
+    //     }
+    //     prodToUpadate.categoria = req.body.categoria;
+    //     prodToUpadate.medidas = req.body.medidas;
+    //     prodToUpadate.alto = req.body.alto;
+    //     prodToUpadate.ancho = req.body.ancho;
+    //     prodToUpadate.profundidad = req.body.profundidad;
+    //     prodToUpadate.color = req.body.color;
+    //     prodToUpadate.fechaCreacion = products[i].fechaCreacion;
+    //     prodToUpadate.fechaModificacion = new Date();
+    //     products[i] = prodToUpadate;
+    //     break;
+    //   }
+    // }
+    // grabaRegistros(products);
+    // res.render("productDescription", { selectedProduct: prodToUpadate });
+      
+    //
+    // db.Product
+    // .update(
+    //     {
+    //       description: req.body.descripcion,
+    //       brand: null,
+    //       creation_date: null,
+    //       modif_date: null,
+    //       active: 1,
+    //       id_colors: req.body.color,
+    //       id_prod_category: req.body.categoria,
+    //       id_prod_price: req.body.precio,
+    //     },
+    //     {
+    //         where: {id_product: productId}
+    //     })
+    // .then(()=> {
+    //     return res.redirect('/')})            
+    // .catch(error => res.send(error))
+    let productId = req.params.id;
+    db.Product.findOne({where: {id_product: 1}})
+.then(record => {
+  
+  if (!record) {
+    throw new Error('No record found')
+  }
+
+  console.log(`retrieved record ${JSON.stringify(record,null,2)}`) 
+
+  let values = {
+    description: req.body.descripcion,
+          brand: null,
+          creation_date: null,
+          modif_date: null,
+          active: 1,
+          id_colors: req.body.color,
+          id_prod_category: req.body.categoria,
+          id_prod_price: req.body.precio,
+  }
+  
+  record.update(values).then( updatedRecord => {
+    console.log(`updated record ${JSON.stringify(updatedRecord,null,2)}`)
+    // login into your DB and confirm update
+  })
+
+})
+.catch((error) => {
+  // do seomthing with the error
+  throw new Error(error)
+})
+
   },
   delete: (req, res) => {
     //
