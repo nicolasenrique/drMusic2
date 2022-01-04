7eeb74ebd6af406eff4d7adf33118aa7c5032c4c
eferreccio <estebanferreccio@gmail.con>
2022-01-03 22:57:25 -0300

    field by field validation, register form

--- public/js/registerFormValidation.js
+++ public/js/registerFormValidation.js
@@ -7,6 +7,117 @@ window.onload = function(){
     let fieldPassword = document.querySelector('input.password');
     let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
 
+    //Validando el campo Nombre
+    
+    let errorName = document.querySelector(".errorName p");    
+    
+    fieldName.addEventListener('focus', function () {
+        
+        errorName.innerHTML += "El campo nombre no debe estar vacío";        
+    })
+
+    fieldName.onkeydown  = function () {
+   
+        errorName.innerHTML = "";           
+    }
+
+    fieldName.onblur  = function () {
+
+        if (fieldName.value.length < 2 ) {
+
+            errorName.innerHTML = "El campo nombre de poseer al menos 2 caracteres";  
+
+        } else {
+
+            errorName.innerHTML = "";
+        }
+                 
+    }
+
+    //Validando el campo Apellido
+    
+    let errorLastName = document.querySelector(".errorLastName p");    
+    
+    fieldLastName.addEventListener('focus', function () {
+            
+        errorLastName.innerHTML += "El campo apellido no debe estar vacío";        
+    })
+    
+    fieldLastName.onkeydown  = function () {
+       
+        errorLastName.innerHTML = "";           
+    }
+    
+    fieldLastName.onblur  = function () {
+    
+        if (fieldLastName.value.length < 2 ) {
+    
+            errorLastName.innerHTML = "El campo nombre de poseer al menos 2 caracteres";  
+    
+        } else {
+    
+            errorLastName.innerHTML = "";
+        }
+                     
+    }
+
+    //Validando el campo Email
+    
+    let errorEmail = document.querySelector(".errorEmail p");    
+    
+    fieldEmail.addEventListener('focus', function () {
+            
+        errorEmail.innerHTML += "El campo email no debe estar vacío";        
+    })
+    
+    fieldEmail.onkeydown  = function () {
+       
+        errorEmail.innerHTML = "";           
+    }
+    
+    fieldEmail.onblur  = function () {
+    
+        if (regex.test(fieldEmail.value) == false) {
+
+            errorEmail.innerHTML = "debes escribir un email válido";
+
+        } else {
+    
+            errorEmail.innerHTML = "";
+        }
+                     
+    }
+
+    //Validando el campo Password
+    
+    let errorPassword = document.querySelector(".errorPassword p");    
+    
+    fieldPassword.addEventListener('focus', function () {
+            
+        errorPassword.innerHTML += "El campo password no debe estar vacío";        
+    })
+    
+    fieldPassword.onkeydown  = function () {
+       
+        errorPassword.innerHTML = "";           
+    }
+    
+    fieldPassword.onblur  = function () {
+    
+        if (fieldPassword.value.length < 8 ) {
+    
+            errorPassword.innerHTML = "La contraseña debe poseer al menos 8 caracteres";  
+    
+        } else {
+    
+            errorPassword.innerHTML = "";
+        }
+                     
+    }
+    
+    /* 
+    *** La forma que probé inicialmente, la misma funcionaba,
+         pero acumulaba los mensajes de errores ***
 
     form.addEventListener("submit", function(event){
 
@@ -73,7 +184,7 @@ window.onload = function(){
 
         }
 
-    });
+    }); */
 
 
     
